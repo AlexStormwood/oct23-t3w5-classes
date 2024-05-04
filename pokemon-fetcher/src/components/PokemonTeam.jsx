9aca34f41a21cc14f98f90414dcf39757eeab062
Alex H <alex.holder@bigfootds.com>
2024-05-04 11:12:28 +1000

    fix: make 6 requests instead of 1

--- pokemon-fetcher/src/components/PokemonTeam.jsx
+++ pokemon-fetcher/src/components/PokemonTeam.jsx
@@ -23,24 +23,34 @@ export default class PokemonTeam extends React.Component {
 			return Math.floor(Math.random() * 1025) + 1;
 		}
 
-		let randomNumber = getRandomPokemonId();
-		console.log(randomNumber);
+		for (let index = 0; index < 6; index++){
+			let randomNumber = getRandomPokemonId();
+			console.log(randomNumber);
 
-		let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber).catch(error => {
-			console.log(error);
-			return error;
-		});
-		let data = await response.json();
-		console.log(data.name);
+			let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber).catch(error => {
+				console.log(error);
+				return error;
+			});
+			let data = await response.json();
+			console.log(data.name);
 
-		// Invalid way to update state:
-		// this.state.pokemonTeam.push(data)
-		// Valid way to update state:
-		this.setState({pokemonTeam: [...this.state.pokemonTeam, data]});
+			// Invalid way to update state:
+			// this.state.pokemonTeam.push(data)
+			// Valid way to update state:
+			this.setState({pokemonTeam: [...this.state.pokemonTeam, data]});
+			// this.setState((state) => {
+			// 	let stateCopy = JSON.parse(JSON.stringify(this.state.pokemonTeam));
+			// 	let newTeam = [...stateCopy, data];
 
-		// console.log("UUID example");
-		// console.log(crypto.randomUUID());
+			// 	return {pokemonTeam: newTeam}
+			// });
+
+			// console.log("UUID example");
+			// console.log(crypto.randomUUID());
+		}
 	}
+
+	
 
 	// Store data from each request
 
